28e21a515cda6892193822dde9bc7c72cb1bff0d
Kunal Rohilla <93761501+Kunall04@users.noreply.github.com>
2025-10-30 14:29:32 +0530

    jwt

--- auth/index.js
+++ auth/index.js
@@ -1,27 +1,30 @@
 const express=require('express');
 const app=express();
 // const axios=require('axios');
+const jwt=require('jsonwebtoken');
+const JWT_SECRET="forwhomthebelltolls";
+
 
 app.use(express.json()); //helps you parse the req's body.
 
 const users=[];
 
-function generateToken() {
-  let options = [
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
-    'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
-  ];
+// function generateToken() {
+//   let options = [
+//     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 
+//     'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
+//     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 
+//     'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 
+//     '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+//   ];
 
-  let token = "";
-  for (let i = 0; i < 32; i++) {
-    token = token + options[Math.floor(Math.random() * options.length)];
-  }
+//   let token = "";
+//   for (let i = 0; i < 32; i++) {
+//     token = token + options[Math.floor(Math.random() * options.length)];
+//   }
 
-  return token;
-}
+//   return token;
+// }
 
 
 app.post("/signup", function(req,res) {
@@ -61,13 +64,18 @@ app.post("/signin", (req, res) => {
     const user = users.find(user => user.username === username && user.password === password);
 
     if (user) {
-        const token = generateToken();
-        user.token = token;
+        // const token = generateToken();
+        const token=jwt.sign({
+            username: username
+        }, JWT_SECRET)
+
+        // user.token=token;
         res.send({
-            token
+            token: token
         })
         console.log(users);
-    } else {
+    } 
+    else {
         res.status(403).send({
             message: "Invalid username or password"
         })
@@ -76,16 +84,19 @@ app.post("/signin", (req, res) => {
 
 app.get("/me", function(req,res) {
     const token=req.headers.token;
-    const user=users.find(u=> u.token===token);
+    const userInfo=jwt.verify(token,JWT_SECRET); //prove the json: { username: username}
+    const username=userInfo.username;
+    const user=users.find(u=> u.username===username);
 
     if(user) {
         res.send({
-            username: user.username
+            username: user.username,
+            password: user.password
         });
     }
     else {
         res.status(401).send({
-            message: "invalid token/unauthorized"
+            message: "unauthorized"
         });
     }
 });
