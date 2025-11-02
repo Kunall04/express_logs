3f66a293bf772a364f31b296a30661f01e7af88d
Kunal Rohilla <93761501+Kunall04@users.noreply.github.com>
2025-11-02 23:52:25 +0530

    aef

--- fstack/index.js
+++ fstack/index.js
@@ -88,7 +88,7 @@ app.post("/signin", (req, res) => {
     } 
     else {
         res.status(403).send({
-            message: "Invalid user"
+            message: "Invalid username"
         })
     }
 });
