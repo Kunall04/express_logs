a2ca813515bcc2ac719076fd09ece26883c755db
Kunal Rohilla <93761501+Kunall04@users.noreply.github.com>
2025-11-23 00:16:39 +0530

    sch

--- manGo/db.js
+++ manGo/db.js
@@ -1,2 +1,26 @@
 const mongoose=require('mongoose');
-const Schema=mongoose.Schema
+const Schema=mongoose.Schema;
+const ObjectId=mongoose.ObjectId;
+
+//making a schema
+const User=new Schema({
+    name: String,
+    email: String,
+    password: String
+});
+
+const Todo=new Schema({
+    userId: ObjectId,
+    title: String,
+    done: Boolean
+});
+
+//model
+const usermodel=mongoose.model('user',User);
+const todomodel=mongoose.model('todo',Todo);
+
+//exporting
+module.exports={
+    usermodel,
+    todomodel
+}
