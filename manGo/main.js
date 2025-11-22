a536b0f9fe5bd5016d8b512207d882888f89128a
Kunal Rohilla <93761501+Kunall04@users.noreply.github.com>
2025-11-23 01:02:37 +0530

    smth

---
+++ manGo/main.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const { usermodel, todomodel } = require('./db.js');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = "nigga leave me alone";
+const app = express();
+
+app.use(express.json());
+
+app.post('/signup', async function(req, res) {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+        const name = req.body.name;
+
+        await usermodel.insertMany({
+            name: name, 
+            email: email,
+            password: password
+        });
+
+        res.json({
+            message: "signed up"
+        });
+    } catch (error) {
+        res.status(400).json({
+            message: "Error signing up",
+            error: error.message
+        });
+    }
+});
+
+app.post('/signin', async function(req, res) {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        const user = await usermodel.findOne({
+            email: email,
+            password: password
+        });
+
+        if(user) {
+            const token = jwt.sign({
+                id: user._id
+            }, JWT_SECRET);  // Fixed: added JWT_SECRET as second parameter
+            
+            res.json({
+                token: token
+            });
+        }
+        else {
+            res.status(403).json({
+                message: "invalid credentials"
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            message: "Error signing in",
+            error: error.message
+        });
+    }
+});
+
+app.listen(3007);
